dac68eb630733f707cb56e91b8e0602bfa50d297
saikumar2627 <129208729+saikumar2627@users.noreply.github.com>
2023-11-21 12:39:43 +0530

    Add files via upload

---
+++ Account.py
@@ -0,0 +1,51 @@
+# coding=System
+
+class Account(object):
+
+  """
+   
+
+  :version:
+  :author:
+  """
+
+  """ ATTRIBUTES
+
+   
+
+  borrow_book  (private)
+
+   
+
+  reserved_book  (private)
+
+   
+
+  return_book  (private)
+
+   
+
+  fine_amount  (public)
+
+  """
+
+  def calculate_price(self):
+    """
+     
+
+    @return  :
+    @author
+    """
+    pass
+
+  def calculate_fine(self):
+    """
+     
+
+    @return  :
+    @author
+    """
+    pass
+
+
+
